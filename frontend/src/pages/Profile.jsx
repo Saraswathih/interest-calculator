be1ae1e5ad0bb30a2e47e7c717c0e2b1028d24f2
Saraswathih <saraswathih163@gmail.com>
2026-03-09 20:20:00 +0530

    fix vercel build error

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/profile.css";
+import "../styles/Profile.css";
 
 export default function Profile() {
   const navigate = useNavigate();
